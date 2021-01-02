0311015d92ab6ba87f073551bc21819d2e3daf35
jegasus <phil.ixo@gmail.com>
2021-01-01 22:31:16 -0600

    FINALLY GOT IT TO WORK

--- scripts/journal-scaler.js
+++ scripts/journal-scaler.js
@@ -58,12 +58,62 @@ Hooks.once('init', async function() {
 });
 
 function _onWheel_override(event){
-  console.log("NOW WE'RE REALLY IN THERE, BABY!!!!");
-  
-  let textElement = document.getElementById('journal-4haVU7wRmVLVYru8');
+  //console.log("NOW WE'RE REALLY IN THERE, BABY!!!!");
 
-  console.log(textElement.style.fontSize);
+  // Check if control is pressed.
+  // If not, just exit the function.
+  if (event.ctrlKey == false) { return; };
+
+  let jrn_sheet_windows = document.getElementsByClassName('app window-app sheet journal-sheet');
   
+  // Check if there are ANY journal sheet windows open.
+  // If there are none, just exit the function.
+  if (jrn_sheet_windows.length == 0) { return; };
+  
+  // Trying to find the window hovered with the mouse 
+  // among the opened journal sheet windows.
+  let i;
+  let journal_win;
+  let foundit = false;
+  for (i in jrn_sheet_windows) {
+    journal_win = jrn_sheet_windows[i];
+    if (journal_win.contains(event.target)){
+      foundit = true;
+      break;
+    }
+  };
+  
+  // If the window hovered with the mouse isn't found among the all of the
+  // open journal sheet windows, just exit the program.
+  if (foundit == false) { return; };
+  
+  // If the code reaches this point, we know that the hovered window is a 
+  // journal sheet window, and that the ctrl key was pressed when scrolling.
+  // Therefore, we can increase/decrease the font size!
+  
+  // Find the direction of the mousewheel scroll
+  let which_dir = '';
+  if (event.wheelDelta > 0) {
+    which_dir = 'bigger';
+  }
+  else if (event.wheelDelta < 0) {
+    which_dir = 'smaller';
+  }
+
+  // Get the DOM element of the journal editor and change its style
+  let journal_editor = journal_win.getElementsByClassName('editor-content')[0]
+  let current_size_str = journal_editor.style.fontSize
+  let current_size = current_size_str == '' ? 14 : Number(current_size_str.slice(0,current_size_str.length-2));
+  if (which_dir == 'bigger') {
+    journal_editor.style.fontSize = `${String(current_size + 1)}px`
+  }
+  else {
+    journal_editor.style.fontSize = `${String(current_size - 1)}px`
+  }
+  
+  //let journal_editor = journal_win.getElementsByClassName('editor-content')[0];
+  //journal_win.contains(event.target)
+  //console.log(textElement.style.fontSize);
   return
 }
 
@@ -75,6 +125,7 @@ Hooks.once('setup', function () {
           event.preventDefault();
           console.log("I'M IN, BABY! BOO-MUTHA-FUCKIN'-YAAA!!!")
           _onWheel_override.bind(this)(event);
+
           //const element = event.currentTarget;
           //const entityId = element.parentElement.dataset.entityId;
           //const entity = this.constructor.collection.get(entityId);

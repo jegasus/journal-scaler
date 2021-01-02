c2898fae30d29bace47bced3815075c9b64183a5
jegasus <phil.ixo@gmail.com>
2021-01-01 22:39:59 -0600

    cleanup

--- scripts/journal-scaler.js
+++ scripts/journal-scaler.js
@@ -7,55 +7,14 @@ function getSetting (settingName) {
   return game.settings.get(MODULE_ID, settingName)
 }
 
-
-
-CONFIG.debug.hooks = true;
+//CONFIG.debug.hooks = true;
 
 console.log("IF YOU USE THIS MODULE YOU WILL VOMIT A WEDDING")
-console.log("BUT SERIOUSLY THO?!?!??! FUUUUUUUUUCK")
-
 
 // Need to edit this function:
 // KeyboardManager._onWheel, line 2834 from foundry.js
-// 
-
-
 
 const MAX_ZOOM_OUT = 0.1;
-
-//let textElement = document.getElementById('text-annotation');
-//let zoom = 1;
-
-/*
-function renderText() {
-    textElement.innerText = zoom;
-};
-*/
-//function onMouseWheel(e) {
-//  const delta = (e.wheelDelta && e.wheelDelta / 40) || -e.detail;
-//  zoom = Math.max(MAX_ZOOM_OUT, zoom + (delta / 100))
-//  renderText();
-//}
-
-//document.addEventListener('mousewheel', onMouseWheel)
-//document.addEventListener('DOMMouseScroll', onMouseWheel) // FirefoxrenderText();
-
-/*
-function renderText() {
-  const preciseFontSize = FONT_SIZE * zoom;  // Desired font size
-  const roundedSize = Math.floor(preciseFontSize);
-  const s = preciseFontSize / roundedSize; // Remaining scale
-  
-  textElement.style.fontSize = `${roundedSize}px`;  
-  const scale = `scale(${s})`;  
-  textElement.style.transform = scale;
-}
-*/
-
-
-Hooks.once('init', async function() {
-
-});
 
 function _onWheel_override(event){
   //console.log("NOW WE'RE REALLY IN THERE, BABY!!!!");
@@ -110,10 +69,6 @@ function _onWheel_override(event){
   else {
     journal_editor.style.fontSize = `${String(current_size - 1)}px`
   }
-  
-  //let journal_editor = journal_win.getElementsByClassName('editor-content')[0];
-  //journal_win.contains(event.target)
-  //console.log(textElement.style.fontSize);
   return
 }
 
@@ -123,23 +78,12 @@ Hooks.once('setup', function () {
       'KeyboardManager.prototype._onWheel', 
         function(existing_onWheel, event) {
           event.preventDefault();
-          console.log("I'M IN, BABY! BOO-MUTHA-FUCKIN'-YAAA!!!")
+          //console.log("I'M IN, BABY! BOO-MUTHA-FUCKIN'-YAAA!!!")
           _onWheel_override.bind(this)(event);
-
-          //const element = event.currentTarget;
-          //const entityId = element.parentElement.dataset.entityId;
-          //const entity = this.constructor.collection.get(entityId);
-  
-          //if (entity.entity == "Scene") {
-          //  entity.view();
-          //}
-          //else return existing_onWheel.bind(this)(event)
           return existing_onWheel.bind(this)(event);
       },
       'MIXED',
     )
-  })
+  }
+)
 
-  Hooks.once('ready', async function() {
-
-  });

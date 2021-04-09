7f43ac6fb0346e203cfa4fd47a0434a756a6df55
jegasus <phil.ixo@gmail.com>
2021-04-08 19:48:50 -0500

    minor cosmetic edits

--- scripts/journal-scaler.js
+++ scripts/journal-scaler.js
@@ -8,93 +8,9 @@ function getSetting (settingName) {
 }
 
 //CONFIG.debug.hooks = true;
-
 console.log("IF YOU USE THIS MODULE YOU WILL VOMIT A WEDDING!!!!")
 
-// Increases font size of journal entries depending on the direction
-function _onWheel_textResize(journal_win, which_dir) {
-  // Get the DOM element of the journal editor and change its style
-  let journal_editor = journal_win.getElementsByClassName('editor-content')[0]
-  let current_size_str = journal_editor.style.fontSize
-  let current_size = current_size_str == '' ? 14 : Number(current_size_str.slice(0,current_size_str.length-2));
-  if (which_dir == 'bigger') {
-    journal_editor.style.fontSize = `${String(current_size + 1)}px`
-  }
-  else {
-    journal_editor.style.fontSize = `${String(current_size - 1)}px`
-  }
-}
-
-// Increases image size of image journal entries depending on the direction
-function _onWheel_imageResize(journal_win, which_dir) {
-  // Get the DOM element of the journal editor and change its style
-  let journal_image = journal_win.getElementsByClassName('lightbox-image')[0];
-  let current_size_str = journal_image.style.backgroundSize;
-  let current_size = current_size_str.includes("%") ? Number(current_size_str.slice(0, current_size_str.length-1)) : 100;
-  if (which_dir == 'bigger') {
-    journal_image.style.backgroundSize = `${String(current_size + 5)}%`
-  }
-  else {
-    journal_image.style.backgroundSize = `${String(current_size - 5)}%`
-  }
-}
-
-// Need to edit this function:
-// KeyboardManager._onWheel, line 2834 from foundry.js
-
-function _onWheel_override(event){
-  // Check if control is pressed.
-  // If not, just exit the function.
-  if (event.ctrlKey == false) { return; };
-
-  // Get the list of all windows currently open that are journal-sheet windows
-  let jrn_sheet_windows = document.getElementsByClassName('sheet journal-sheet');
-
-  // If there are none, just exit the function.
-  if (jrn_sheet_windows.length == 0) {
-    return;
-  };
-
-  // Trying to find the window hovered with the mouse
-  // among the opened journal sheet windows.
-  let i;
-  let journal_win;
-  let foundit = false;
-  for (i = 0; i < jrn_sheet_windows.length; i++){
-    journal_win = jrn_sheet_windows[i];
-    if (journal_win.contains(event.target)){
-      foundit = true;
-      break;
-    }
-  };
-
-  // If the window hovered with the mouse isn't found among the all of the
-  // open journal sheet windows, just exit the program.
-  if (foundit == false) { return; };
-
-  // If the code reaches this point, we know that the hovered window is a
-  // journal sheet window, and that the ctrl key was pressed when scrolling.
-  // Therefore, we can increase/decrease the font size!
-
-  // Find the direction of the mousewheel scroll
-  let which_dir = '';
-  if ((event.wheelDelta > 0) | (event.deltaY < 0)){
-    which_dir = 'bigger';
-  }
-  else if ((event.wheelDelta < 0) | (event.deltaY > 0)) {
-    which_dir = 'smaller';
-  }
-
-  // Check if this is an image journal entry
-  if (journal_win.querySelector('.lightbox-image')) {
-    _onWheel_imageResize(journal_win, which_dir);
-  } else {
-    _onWheel_textResize(journal_win, which_dir);
-  }
-
-  return
-}
-
+// Overriding original mousewheel behavior
 Hooks.once('setup', function () {
     libWrapper.register(
       MODULE_ID,
@@ -150,3 +66,88 @@ Hooks.on('renderJournalSheet', function() {
     });
   }
 })
+
+
+// Function that overrides the original mousewheel behavior.
+// Original function: KeyboardManager._onWheel, line 2834 from foundry.js
+function _onWheel_override(event){
+  // Check if control is pressed.
+  // If not, just exit the function.
+  if (event.ctrlKey == false) { return; };
+
+  // Get the list of all windows currently open that are journal-sheet windows
+  let jrn_sheet_windows = document.getElementsByClassName('sheet journal-sheet');
+
+  // If there are none, just exit the function.
+  if (jrn_sheet_windows.length == 0) {
+    return;
+  };
+
+  // Trying to find the window hovered with the mouse
+  // among the opened journal sheet windows.
+  let i;
+  let journal_win;
+  let foundit = false;
+  for (i = 0; i < jrn_sheet_windows.length; i++){
+    journal_win = jrn_sheet_windows[i];
+    if (journal_win.contains(event.target)){
+      foundit = true;
+      break;
+    }
+  };
+
+  // If the window hovered with the mouse isn't found among the all of the
+  // open journal sheet windows, just exit the program.
+  if (foundit == false) { return; };
+
+  // If the code reaches this point, we know that the hovered window is a
+  // journal sheet window, and that the ctrl key was pressed when scrolling.
+  // Therefore, we can increase/decrease the font size!
+
+  // Find the direction of the mousewheel scroll
+  let which_dir = '';
+  if ((event.wheelDelta > 0) | (event.deltaY < 0)){
+    which_dir = 'bigger';
+  }
+  else if ((event.wheelDelta < 0) | (event.deltaY > 0)) {
+    which_dir = 'smaller';
+  }
+
+  // Check if this is an image journal entry or just regular text
+  if (journal_win.querySelector('.lightbox-image')) {
+    _onWheel_imageResize(journal_win, which_dir);
+  } else {
+    _onWheel_textResize(journal_win, which_dir);
+  }
+
+  return
+}
+
+
+// Increases font size of journal entries depending on the direction
+function _onWheel_textResize(journal_win, which_dir) {
+  // Get the DOM element of the journal editor and change its style
+  let journal_editor = journal_win.getElementsByClassName('editor-content')[0]
+  let current_size_str = journal_editor.style.fontSize
+  let current_size = current_size_str == '' ? 14 : Number(current_size_str.slice(0,current_size_str.length-2));
+  if (which_dir == 'bigger') {
+    journal_editor.style.fontSize = `${String(current_size + 1)}px`
+  }
+  else {
+    journal_editor.style.fontSize = `${String(current_size - 1)}px`
+  }
+}
+
+// Increases image size of image journal entries depending on the direction
+function _onWheel_imageResize(journal_win, which_dir) {
+  // Get the DOM element of the image and change its style
+  let journal_image = journal_win.getElementsByClassName('lightbox-image')[0];
+  let current_size_str = journal_image.style.backgroundSize;
+  let current_size = current_size_str.includes("%") ? Number(current_size_str.slice(0, current_size_str.length-1)) : 100;
+  if (which_dir == 'bigger') {
+    journal_image.style.backgroundSize = `${String(current_size + 5)}%`
+  }
+  else {
+    journal_image.style.backgroundSize = `${String(current_size - 5)}%`
+  }
+}
